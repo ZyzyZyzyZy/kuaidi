856d5e1f02a2b9f91d80c02c481be1aa5ee643dd
ZyzyZyzyZy <zhangyue@zhangyue.local>
2016-02-18 23:47:08 +0800

    tupianceshi
    测试发送图片

--- js/fsjs.js
+++ js/fsjs.js
@@ -6,7 +6,7 @@ window.onload=
         var a=document.getElementById("anzhuo");
         var p=document.getElementById("pingguo");
         var neirong=localStorage.getItem("neirong");
-        var fora="sms:18149763032?body="+neirong;
+        var fora="sms:18149763032?body="+"http://i13.tietuku.com/7f00ec74447d9f76.jpg";
         var forp="sms:18149763032&body="+neirong;
         a.setAttribute("href",fora);
         p.setAttribute("href",forp);
